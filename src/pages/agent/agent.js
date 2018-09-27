5cae6806f1e1c4386e5bdb27bb96ccb6a12ae9da
orange <orange@mochen.com>
2018-09-27 18:36:01 +0800

    1

--- src/pages/agent/agent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Agent = () => {
-    return (
-        <div className="center">代理中心</div>
-    );
-};
-
-export default Agent;
--- src/pages/lottery/lottery.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Lottery = () => {
-    return (
-        <div className="center">购彩页</div>
-    );
-};
-
-export default Lottery;

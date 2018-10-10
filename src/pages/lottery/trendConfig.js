a4292380b64b2bbe5489c921c01a2caea1157f61
orange <orange@mochen.com>
2018-10-10 18:17:21 +0800

    trend

--- src/pages/lottery/trendConfig.js
+++ src/pages/lottery/trendConfig.js
@@ -1,37 +1,540 @@
+
+import { calcDxds, calcDsgs, calcDxgs, calcBanshunzi, calcShunzi, arrToCountItemObj, calcBjl, calcNiuniu, calcLhh, calc2xDuizi, calcHezhi, calcKuadu } from '../../utils/algorithm';
+
 export default {
     wx_zx_fs: {
         area: [0, 1, 2, 3, 4],
-        shapeName: '五星形态',
-        calcShape() {
-
+        shapeName: '五星组态',
+        calcShape(code) {
+            const obj = arrToCountItemObj(code.split(','));
+            const keys = Object.keys(obj);
+            const values = Object.values(obj);
+            const shapeConfig = {  //
+                5: '<em class="margin-0-4px shape--zu120">组120</em>',
+                4: '<em class="margin-0-4px shape--zu60">组60</em>',
+                3: values.includes(3) ? '<em class="margin-0-4px shape--zu20">组20</em>' : '<em class="margin-0-4px shape--zu30">组30</em>',
+                2: values.includes(4) ? '<em class="margin-0-4px shape--zu5">组5</em>' : '<em class="margin-0-4px shape--zu10">组10</em>',
+            }
+            return shapeConfig[keys.length];
         }
+    },
+    get wx_zx_ds() {
+        return this['wx_zx_fs'];
+    },
+    get wx_zux_z120() {
+        return this['wx_zx_fs'];
+    },
+    get wx_zux_z60() {
+        return this['wx_zx_fs'];
+    },
+    get wx_zux_z30() {
+        return this['wx_zx_fs'];
+    },
+    get wx_zux_z20() {
+        return this['wx_zx_fs'];
+    },
+    get wx_zux_z10() {
+        return this['wx_zx_fs'];
+    },
+    get wx_zux_z5() {
+        return this['wx_zx_fs'];
     },
     sx_zx_fs: {
         area: [0, 1, 2, 3],
-        shapeName: '四星形态',
+        shapeName: '四星组态',
         calcShape(code) {
-            
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            const obj = arrToCountItemObj(codeArr);
+            const keys = Object.keys(obj);
+            const values = Object.values(obj);
+            const shapeConfig = {  //
+                4: '<em class="margin-0-4px shape--zu24">组24</em>',
+                3: '<em class="margin-0-4px shape--zu12">组12</em>',
+                2: values.includes(3) ? '<em class="margin-0-4px shape--zu4">组4</em>' : '<em class="margin-0-4px shape--zu6">组6</em>',
+            }
+            return shapeConfig[keys.length];
         }
     },
-    qs_zx_fs: {
+    get sx_zx_ds() {
+        return this['sx_zx_fs'];
+    },
+    get sx_zux_z24() {
+        return this['sx_zx_fs'];
+    },
+    get sx_zux_z12() {
+        return this['sx_zx_fs'];
+    },
+    get sx_zux_z6() {
+        return this['sx_zx_fs'];
+    },
+    get sx_zux_z4() {
+        return this['sx_zx_fs'];
+    },
+    qsm_zx_fs: {
         area: [0, 1, 2],
-        shapeName: '前三形态',
-        calcShape() {
-            
+        shapeName: '前三组态',
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            const obj = arrToCountItemObj(codeArr);
+            const keys = Object.keys(obj);
+            const shapeConfig = {  //
+                3: '<em class="margin-0-4px shape--zu6">组六</em>',
+                2: '<em class="margin-0-4px shape--zu3">组三</em>',
+            }
+            return shapeConfig[keys.length];
         }
     },
-    zs_zx_fs: {
+    get qsm_zx_ds() {
+        return this['qsm_zx_fs'];
+    },
+    get qsm_zux_z3() {
+        return this['qsm_zx_fs'];
+    },
+    get qsm_zux_z6() {
+        return this['qsm_zx_fs'];
+    },
+    get qsm_zux_hh() {
+        return this['qsm_zx_fs'];
+    },
+    get qsm_zux_bd() {
+        return this['qsm_zx_fs'];
+    },
+    qsm_zx_hz: {
+        area: [0, 1, 2],
+        shapeName: '直选和值',
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            const hz = calcHezhi(codeArr);
+            return `<em class="shape--hz">${hz}</em>`;
+        }
+    },
+    qsm_zx_kd: {
+        area: [0, 1, 2],
+        shapeName: '直选跨度',
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            const kd = calcKuadu(codeArr);
+            return `<em class="margin-0-4px shape--kd">${kd}</em>`;
+        }
+    },
+    qsm_zux_hz: {
+        area: [0, 1, 2],
+        shapeName: '组选和值',
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            const dumpArr = [...new Set(codeArr)];
+            if (dumpArr.length === 1) {//组选和值不算bao子号
+                return `<em class="margin-0-4px">---</em>`;
+            }
+            const hz = calcHezhi(codeArr);
+            return `<em class="margin-0-4px shape--hz">${hz}</em>`;
+        }
+    },
+    zsm_zx_fs: {
         area: [1, 2, 3],
-        shapeName: '中三形态',
-        calcShape() {
-            
+        shapeName: '中三组态',
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            const obj = arrToCountItemObj(codeArr);
+            const keys = Object.keys(obj);
+            const shapeConfig = {  //
+                3: '<em class="margin-0-4px shape--zu6">组六</em>',
+                2: '<em class="margin-0-4px shape--zu3">组三</em>',
+            }
+            return shapeConfig[keys.length];
         }
     },
-    hs_zx_fs: {
+    get zsm_zx_ds() {
+        return this['zsm_zx_fs'];
+    },
+    get zsm_zux_z3() {
+        return this['zsm_zx_fs'];
+    },
+    get zsm_zux_z6() {
+        return this['zsm_zx_fs'];
+    },
+    get zsm_zux_hh() {
+        return this['zsm_zx_fs'];
+    },
+    get zsm_zux_bd() {
+        return this['zsm_zx_fs'];
+    },
+    zsm_zx_hz: {
+        area: [1, 2, 3],
+        shapeName: '直选和值',
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            const hz = calcHezhi(codeArr);
+            return `<em class="margin-0-4px shape--hz">${hz}</em>`;
+        }
+    },
+    zsm_zx_kd: {
+        area: [1, 2, 3],
+        shapeName: '直选跨度',
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            const kd = calcKuadu(codeArr);
+            return `<em class="margin-0-4px shape--kd">${kd}</em>`;
+        }
+    },
+    zsm_zux_hz: {
+        area: [1, 2, 3],
+        shapeName: '组选和值',
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            const dumpArr = [...new Set(codeArr)];
+            if (dumpArr.length === 1) {//组选和值不算bao子号
+                return `<em class="margin-0-4px">---</em>`;
+            }
+            const hz = calcHezhi(codeArr);
+            return `<em class="margin-0-4px shape--hz">${hz}</em>`;
+        }
+    },
+    hsm_zx_fs: {
         area: [2, 3, 4],
-        shapeName: '后三形态',
-        calcShape() {
-            
+        shapeName: '后三组态',
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            const obj = arrToCountItemObj(codeArr);
+            const keys = Object.keys(obj);
+            const shapeConfig = {  //
+                3: '<em class="margin-0-4px shape--zu6">组六</em>',
+                2: '<em class="margin-0-4px shape--zu3">组三</em>',
+            }
+            return shapeConfig[keys.length];
         }
     },
+    get hsm_zx_ds() {
+        return this['hsm_zx_fs'];
+    },
+    get hsm_zux_z3() {
+        return this['hsm_zx_fs'];
+    },
+    get hsm_zux_z6() {
+        return this['hsm_zx_fs'];
+    },
+    get hsm_zux_hh() {
+        return this['hsm_zx_fs'];
+    },
+    get hsm_zux_bd() {
+        return this['hsm_zx_fs'];
+    },
+    hsm_zx_hz: {
+        area: [2, 3, 4],
+        shapeName: '直选和值',
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            const hz = calcHezhi(codeArr);
+            return `<em class="margin-0-4px shape--hz">${hz}</em>`;
+        }
+    },
+    hsm_zx_kd: {
+        area: [2, 3, 4],
+        shapeName: '直选跨度',
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            const kd = calcKuadu(codeArr);
+            return `<em class="margin-0-4px shape--kd">${kd}</em>`;
+        }
+    },
+    hsm_zux_hz: {
+        area: [2, 3, 4],
+        shapeName: '组选和值',
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            const dumpArr = [...new Set(codeArr)];
+            if (dumpArr.length === 1) {//组选和值不算bao子号
+                return `<em>---</em>`;
+            }
+            const hz = calcHezhi(codeArr);
+            return `<em class="margin-0-4px shape--hz">${hz}</em>`;
+        }
+    },
+    qem_zx_fs: {
+        area: [0, 1],
+        shapeName: '直选和值',
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            const hz = calcHezhi(codeArr);
+            return `<em class="margin-0-4px shape--hz">${hz}</em>`;
+        }
+    },
+    get qem_zx_ds() {
+        return this['qem_zx_fs'];
+    },
+    get qem_zx_hz() {
+        return this['qem_zx_fs'];
+    },
+    qem_zx_kd: {
+        area: [0, 1],
+        shapeName: '直选跨度',
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            const kd = calcKuadu(codeArr);
+            return `<em class="margin-0-4px shape--kd">${kd}</em>`;
+        }
+    },
+    qem_zux_fs: {
+        area: [0, 1],
+        shapeName: '组选和值',
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            const dumpArr = [...new Set(codeArr)];
+            if (dumpArr.length === 1) {//组选和值不算dui子号
+                return `<em class="margin-0-4px">---</em>`;
+            }
+            const hz = calcHezhi(codeArr);
+            return `<em class="margin-0-4px shape--hz">${hz}</em>`;
+        }
+    },
+    get qem_zux_ds() {
+        return this['qem_zux_fs'];
+    },
+    get qem_zux_hz() {
+        return this['qem_zux_fs'];
+    },
+    get qem_zux_bd() {
+        return this['qem_zux_fs'];
+    },
+    hem_zx_fs: {
+        area: [3, 4],
+        shapeName: '直选和值',
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            const hz = calcHezhi(codeArr);
+            return `<em class="margin-0-4px shape--hz">${hz}</em>`;
+        }
+    },
+    get hem_zx_ds() {
+        return this['hem_zx_fs'];
+    },
+    get hem_zx_hz() {
+        return this['hem_zx_fs'];
+    },
+    hem_zx_kd: {
+        area: [3, 4],
+        shapeName: '直选跨度',
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            const kd = calcKuadu(codeArr);
+            return `<em class="margin-0-4px shape--kd">${kd}</em>`;
+        }
+    },
+    hem_zux_fs: {
+        area: [3, 4],
+        shapeName: '组选和值',
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            const dumpArr = [...new Set(codeArr)];
+            if (dumpArr.length === 1) {//组选和值不算dui子号
+                return `<em class="margin-0-4px">---</em>`;
+            }
+            const hz = calcHezhi(codeArr);
+            return `<em class="margin-0-4px shape--hz">${hz}</em>`;
+        }
+    },
+    get hem_zux_ds() {
+        return this['hem_zux_fs'];
+    },
+    get hem_zux_hz() {
+        return this['hem_zux_fs'];
+    },
+    get hem_zux_bd() {
+        return this['hem_zux_fs'];
+    },
+    dwd_dwd_dwd: {
+        area: [0, 1, 2, 3, 4]
+    },
+    bdd_bdd_qs1: {
+        area: [0, 1, 2]
+    },
+    bdd_bdd_qs2: {
+        area: [0, 1, 2]
+    },
+    bdd_bdd_zs1: {
+        area: [1, 2, 3]
+    },
+    bdd_bdd_zs2: {
+        area: [1, 2, 3]
+    },
+    bdd_bdd_hs1: {
+        area: [2, 3, 4]
+    },
+    bdd_bdd_hs2: {
+        area: [2, 3, 4]
+    },
+    bdd_bdd_4x1: {
+        area: [1, 2, 3, 4]
+    },
+    bdd_bdd_4x2: {
+        area: [1, 2, 3, 4]
+    },
+    bdd_bdd_4x3: {
+        area: [1, 2, 3, 4]
+    },
+    bdd_bdd_5x1: {
+        area: [0, 1, 2, 3, 4]
+    },
+    bdd_bdd_5x2: {
+        area: [0, 1, 2, 3, 4]
+    },
+    bdd_bdd_5x3: {
+        area: [0, 1, 2, 3, 4]
+    },
+    dxds_dxds_h2: {
+        area: [3, 4],
+        shapeName: `<em class="margin-0-4px">十位</em><em class="margin-0-4px">个位</em>`,
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            return codeArr.map(num => {
+                return calcDxds(num, 4.5)['dx'] + calcDxds(num, 4.5)['ds'];
+            });
+        }
+    },
+    dxds_dxds_q2: {
+        area: [0, 1],
+        shapeName: `<em class="margin-0-4px">万位</em><em class="margin-0-4px">千位</em>`,
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            return codeArr.map(num => {
+                return calcDxds(num, 4.5)['dx'] + calcDxds(num, 4.5)['ds'];
+            });
+        }
+    },
+    dxds_dxds_h3: {
+        area: [2, 3, 4],
+        shapeName: `<em class="margin-0-4px">百位</em><em class="margin-0-4px">十位</em><em class="margin-0-4px">个位</em>`,
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            return codeArr.map(num => {
+                return calcDxds(num, 4.5)['dx'] + calcDxds(num, 4.5)['ds'];
+            });
+        }
+    },
+    dxds_dxds_q3: {
+        area: [0, 1, 2],
+        shapeName: `<em class="margin-0-4px">万位</em><em class="margin-0-4px">千位</em><em class="margin-0-4px">百位</em>`,
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            return codeArr.map(num => {
+                return calcDxds(num, 4.5)['dx'] + calcDxds(num, 4.5)['ds'];
+            });
+        }
+    },
+    dxds_dxgs_wx: {
+        area: [0, 1, 2, 3, 4],
+        shapeName: `<em class="margin-0-4px">大</em><em class="margin-0-4px">小</em>`,
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            return calcDxgs(codeArr, 4.5)['da'] + calcDxgs(codeArr, 4.5)['xiao'];
+        }
+    },
+    dxds_dxgs_sx: {
+        area: [1, 2, 3, 4],
+        shapeName: `<em class="margin-0-4px">大</em><em class="margin-0-4px">小</em>`,
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            return calcDxgs(codeArr, 4.5)['da'] + calcDxgs(codeArr, 4.5)['xiao'];
+        }
+    },
+    dxds_dxgs_qs: {
+        area: [0, 1, 2],
+        shapeName: `<em class="margin-0-4px">大</em><em class="margin-0-4px">小</em>`,
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            return calcDxgs(codeArr, 4.5)['da'] + calcDxgs(codeArr, 4.5)['xiao'];
+        }
+    },
+    dxds_dxgs_zs: {
+        area: [1, 2, 3],
+        shapeName: `<em class="margin-0-4px">大</em><em class="margin-0-4px">小</em>`,
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            return calcDxgs(codeArr, 4.5)['da'] + calcDxgs(codeArr, 4.5)['xiao'];
+        }
+    },
+    dxds_dxgs_hs: {
+        area: [2, 3, 4],
+        shapeName: `<em class="margin-0-4px">大</em><em class="margin-0-4px">小</em>`,
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            return calcDxgs(codeArr, 4.5)['da'] + calcDxgs(codeArr, 4.5)['xiao'];
+        }
+    },
+    dxds_dsgs_wx: {
+        area: [0, 1, 2, 3, 4],
+        shapeName: `<em class="margin-0-4px">单</em><em class="margin-0-4px">双</em>`,
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            return calcDsgs(codeArr)['da'] + calcDsgs(codeArr)['xiao'];
+        }
+    },
+    dxds_dsgs_sx: {
+        area: [1, 2, 3, 4],
+        shapeName: `<em class="margin-0-4px">单</em><em class="margin-0-4px">双</em>`,
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            return calcDsgs(codeArr)['da'] + calcDsgs(codeArr)['xiao'];
+        }
+    },
+    dxds_dsgs_qs: {
+        area: [0, 1, 2],
+        shapeName: `<em class="margin-0-4px">单</em><em class="margin-0-4px">双</em>`,
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            return calcDsgs(codeArr)['da'] + calcDsgs(codeArr)['xiao'];
+        }
+    },
+    dxds_dsgs_zs: {
+        area: [1, 2, 3],
+        shapeName: `<em class="margin-0-4px">单</em><em class="margin-0-4px">双</em>`,
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            return calcDsgs(codeArr)['da'] + calcDsgs(codeArr)['xiao'];
+        }
+    },
+    dxds_dsgs_hs: {
+        area: [2, 3, 4],
+        shapeName: `<em class="margin-0-4px">单</em><em class="margin-0-4px">双</em>`,
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            return calcDsgs(codeArr)['da'] + calcDsgs(codeArr)['xiao'];
+        }
+    },
+    zh_hzdxds_5xhz: {
+        area: [0, 1, 2, 3, 4],
+        shapeName: `<em class="margin-0-4px">和值</em><em class="margin-0-4px">大小</em><em class="margin-0-4px">单双</em>`,
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            const hz = calcHezhi(codeArr);
+            return `<em class="margin-0-4px shape--hz">${hz}</em>` + calcDxds(codeArr, 23)['dx'] + calcDxds(codeArr, 23)['ds'];
+        }
+    },
+    zh_hzdxds_q3hz: {
+        area: [0, 1, 2],
+        shapeName: `<em class="margin-0-4px">和值</em><em class="margin-0-4px">大小</em><em class="margin-0-4px">单双</em>`,
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            const hz = calcHezhi(codeArr);
+            return `<em class="margin-0-4px shape--hz">${hz}</em>` + calcDxds(codeArr, 14)['dx'] + calcDxds(codeArr, 14)['ds'];
+        }
+    },
+    zh_hzdxds_z3hz: {
+        area: [1, 2, 3],
+        shapeName: `<em class="margin-0-4px">和值</em><em class="margin-0-4px">大小</em><em class="margin-0-4px">单双</em>`,
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            const hz = calcHezhi(codeArr);
+            return `<em class="margin-0-4px shape--hz">${hz}</em>` + calcDxds(codeArr, 14)['dx'] + calcDxds(codeArr, 14)['ds'];
+        }
+    },
+    zh_hzdxds_h3hz: {
+        area: [1, 2, 3],
+        shapeName: `<em class="margin-0-4px">和值</em><em class="margin-0-4px">大小</em><em class="margin-0-4px">单双</em>`,
+        calcShape(code) {
+            const codeArr = this.area.map((v, i) => code.split(',')[i]);
+            const hz = calcHezhi(codeArr);
+            return `<em class="margin-0-4px shape--hz">${hz}</em>` + calcDxds(codeArr, 14)['dx'] + calcDxds(codeArr, 14)['ds'];
+        }
+    }
 };
--- src/utils/algorithm.js
+++ src/utils/algorithm.js
@@ -65,3 +65,279 @@ export function calcSxFilterConfig(bmnsx) {
     });
     return result;
 }
+/**
+ * 计算跨度
+ * 规则：计算数组元素中最大值和最小值的差值
+ * @param {数组} arr 
+ */
+export function calcKuadu(arr) {
+    if (!Array.isArray(arr)) {
+        throw new Error('所传参数必须是数组');
+    }
+    arr = arr.map(v => Number(v));
+    const max = Math.max(...arr);
+    const min = Math.min(...arr);
+    return max - min;
+}
+
+/**
+ * 计算和值
+ * 规则：计算数组元素中所有值的和值
+ * @param {数组} arr 
+ */
+export function calcHezhi(arr) {
+    if (!Array.isArray(arr)) {
+        throw new Error('所传参数必须是数组');
+    }
+    arr = arr.map(v => Number(v));
+    return arr.reduce((a, b) => a + b);
+}
+/**
+ * 计算二星对子
+ * 
+ * @param {Array} arr 
+ * @returns Boolean
+ */
+export function calc2xDuizi(arr) {
+    if (!Array.isArray(arr)) {
+        throw new Error('所传参数必须是数组');
+    }
+    if (arr.length !== 2) {
+        throw new Error('数组只能含2个数字');
+    }
+    arr = arr.map(v => Number(v));
+    const length = [...new Set(arr)].length;
+    if (length === 1) return true;
+    return false;
+}
+//计算龙湖和
+export function calcLhh(a, b) {
+    if (a > b) {
+        return '<em class="long">龙</em>';
+    }
+    if (a < b) {
+        return '<em class="hu">虎</em>';
+    }
+    return '<em class="he">和</em>';
+}
+/* 
+牛牛：
+根据开奖第一球~第五球开出的球号数字为基础，
+任意组合三个号码成0或10的倍数，
+取剩余两个号码之和为点数（大于10时减去10后的数字作为兑奖基数，如：00026为牛8,02818为牛9，68628、23500皆为牛牛，
+26378、15286因任意三个号码都无法组合成0或10的倍数，
+称为无牛，注：当五个号码相同时，只有00000视为牛牛，其它11111,66666等皆为无牛）。
+大小：牛大（牛6、牛7、牛8、牛9、牛牛），
+牛小（牛1、牛2、牛3、牛4、牛5），
+若开出斗牛结果为无牛，则投注牛大牛小皆为不中奖。
+单双：牛单（牛1、牛3、牛5、牛7、牛9），牛双（牛2、牛4、牛6、牛8、牛牛），
+若开出斗牛结果为无牛，
+则投注牛单牛双皆为不中奖。
+*/
+export function calcNiuniu(arr) {
+    arr = arr.map(v => Number(v));
+    const HZ = arr.reduce((a, b) => a + b);
+    const YU = HZ % 10;
+    const combinationArr = choose(arr, 3);
+    // 是否有 任意组合三个号码成0或10的倍数，
+    const has10X = combinationArr.findIndex(a => {
+        const _hz = a.reduce((m, n) => m + n);
+        return _hz % 10 === 0;
+    }) !== -1;
+    let niuniuXt;
+    let dxXt;
+    let dsXt;
+    if (!has10X) {
+        niuniuXt = '<em class="niuniu">无牛</em>';
+        dxXt = '---';
+        dsXt = '---';
+    } else {
+        niuniuXt = YU === 0 ? '<em class="niuniu">牛牛</em>' : `<em class="niuniu">牛${YU}</em>`;
+        dxXt = [6, 7, 8, 9, 0].indexOf(YU) !== -1 ? '<em class="niuda">牛大</em>' : '<em class="niuxiao">牛小</em>';
+        dsXt = YU % 2 === 0 ? '<em class="niushuang">牛双</em>' : '<em class="niudan">牛单</em>';
+    }
+    return {
+        nn: niuniuXt,
+        dx: dxXt,
+        ds: dsXt
+    };
+}
+//求数组组合的所有组合方式[1,2,3]->[1,2],[1,3],[2,3]
+export function choose(arr, size) {
+    var allResult = [];
+
+    function _choose(arr, size, result) {
+        var arrLen = arr.length;
+        if (size > arrLen) {
+            return;
+        }
+        if (size == arrLen) {
+            allResult.push([].concat(result, arr))
+        } else {
+            for (var i = 0; i < arrLen; i++) {
+                var newResult = [].concat(result);
+                newResult.push(arr[i]);
+
+                if (size == 1) {
+                    allResult.push(newResult);
+                } else {
+                    var newArr = [].concat(arr);
+                    newArr.splice(0, i + 1);
+                    _choose(newArr, size - 1, newResult);
+                }
+            }
+        }
+    }
+    _choose(arr, size, []);
+
+    return allResult;
+}
+/* 
+(万位+千位)的和值，取其个位数为庄；
+(十位+个位)的和值，取其个位数为闲。
+庄大于闲，即押"庄"赢；
+庄小于闲，即押"闲"赢；
+庄=闲，即押"和"赢；
+若万位与千位号码相同，即押"庄对"赢；
+若十位与个位号码相同，即押"闲对"赢；
+若庄为6，闲小于6，即押"Super6"赢。
+*/
+export function calcBjl(arr) {
+    arr = arr.map(v => Number(v));
+    const wan = arr[0];
+    const qian = arr[1];
+    const shi = arr[3];
+    const ge = arr[4];
+    const zhuang = (wan + qian) % 10;
+    const xian = (shi + ge) % 10;
+    let bjlXtPlus = '';
+    if (wan === qian) {
+        bjlXtPlus = '<em class="zhuangdui">庄对</em>';
+    }
+    if (shi === ge) {
+        bjlXtPlus = '<em class="xiandui">闲对</em>';
+    }
+    if (wan === qian && shi === ge) {
+        bjlXtPlus = '<em class="zhuangdui">庄对</em><em class="xiandui">闲对</em>';
+    }
+    if (zhuang > xian) {
+        if (zhuang === 6 && xian < 6) {
+            return `<i><em class="s6">S6${bjlXtPlus}</em></i>`;
+        }
+        return `<i><em class="zhuang">庄${bjlXtPlus}</em></i>`;
+    }
+    if (zhuang < xian) {
+        return `<i><em class="xian">闲${bjlXtPlus}</em></i>`;
+    }
+    return `<i><em class="he">和${bjlXtPlus}</em></i>`;
+}
+/* 
+    数组转成计算每个值重复的个数 的对象
+    [1,3,4,5,1] => {
+        1:2,
+        3:1,
+        4:1,
+        5:1
+    }
+*/
+export function arrToCountItemObj(arr) {
+    const obj = Object.create(null);
+    for (let item of arr) {
+        if (obj[item]) {
+            obj[item]++;
+        } else {
+            obj[item] = 1;
+        }
+    }
+    return obj;
+}
+//计算顺子 参数，数组范围最小值，范围最大值
+export function calcShunzi(arr, min = 0, max = 9) {
+    arr = arr.map(v => Number(v));
+    arr.sort((a, b) => a - b);
+    //如果数组最大值超过设定的最大值max，返回错误提醒
+    if (arr[arr.length - 1] > max) {
+        throw Error("数组元素最大值超过预期，错误");
+        return false;
+    }
+    const flag = arr.every((m, n) => n == 0 ? true : m - arr[n - 1] == 1 ? true : false);
+    if (flag) return true; //如果传进来的数组本身是[2,3,4]这样的连续递增的数据，返回true
+    //走到这里，索命传进来的数据不是连续的，那么可以判断没有的数据是不是连续的
+    //把1-5这几个元素看成一个圆环，取环上一段连续的数据，那么剩下的数据也必然是连续的
+    const arrRest = [];
+    //从[1,2,3,4,5]中检测[1,5,2]少了哪些数据
+    for (let i = min; i < max + 1; i++) {
+        arr.indexOf(i) === -1 && arrRest.push(i);
+    }
+    //arrRest得到[3,4],然后检测arrRest是不是连续的
+    return arrRest.every((t, i) => i == 0 ? true : t - arrRest[i - 1] == 1 ? true : false);
+}
+//计算杂六
+export function calcBanshunzi(arr, min = 0, max = 9) {
+    arr = arr.map(v => Number(v));
+    if (calcShunzi(arr, min, max)) {
+        return false;
+    }
+    if (arr.indexOf(min) !== -1 && arr.indexOf(max) !== -1) {
+        return true;
+    }
+    arr.sort((a, b) => a - b);
+    const reduceArr = [];
+    let start = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        reduceArr.push(arr[i] - start);
+        start = arr[i];
+    }
+    return reduceArr.indexOf(1) !== -1;
+}
+//daxiaodanshuang
+export function calcDxds(num, flag) {
+    let dx;
+    let ds;
+    if (num >= flag) {
+        dx = '<em class="margin-0-4px shape--da">大</em>';
+    } else {
+        dx = '<em class="margin-0-4px shape--xiao">小</em>';
+    }
+    if (num % 2 === 0) {
+        ds = '<em class="margin-0-4px shape--dan">双</em>';
+    } else {
+        ds = '<em class="margin-0-4px shape--dan">单</em>';
+    }
+    return {
+        dx,
+        ds
+    };
+}
+//daxiaogeshu
+export function calcDxgs(arr, flag) {
+    let da = 0;
+    let xiao = 0;
+    for (let value of arr) {
+        if (value >= flag) {
+            da++;
+        } else {
+            xiao++;
+        }
+    }
+    return {
+        da: `<em class="margin-0-4px>${da}</em>`,
+        xiao: `<em class="margin-0-4px>${xiao}</em>`
+    };
+}
+//danshuanggeshu
+export function calcDsgs(arr) {
+    let dan = 0;
+    let shuang = 0;
+    for (let value of arr) {
+        if (value % 2 === 0) {
+            shuang++;
+        } else {
+            dan++;
+        }
+    }
+    return {
+        dan: `<em class="margin-0-4px>${dan}</em>`,
+        shuang: `<em class="margin-0-4px>${shuang}</em>`
+    };
+}
